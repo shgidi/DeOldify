e783531a8611c77f5f93c94df634600bc56122e7
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-30 06:12:41 +0000

    deblur

---
+++ train.py
@@ -0,0 +1,21 @@
+# training script by gidi
+import fastai
+from fastai import *
+from fastai.vision import *
+
+from fastai.vision.gan import *
+from fasterai.generators import *
+from fasterai.critics import *
+from fasterai.dataset import *
+from fasterai.loss import *
+from fasterai.save import *
+from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageFile
+
+from fastai.callbacks.tensorboard import *
+
+def train():
+    pass
+
+if __name__=='__main__':
+    pass
